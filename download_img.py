8cb0de5f3fe04bffcbf07d609a16e62a6c007bf0
Jiancheng Li <lijiancheng@sensetime.com>
2017-07-03 14:50:51 +0800

    Update download_img.

--- download_img.py
+++ download_img.py
@@ -1,31 +1,49 @@
 import json
 import os
-from urllib.request import urlretrieve
+import urllib
+from PIL import Image
+import numpy as np
 from concurrent.futures import ThreadPoolExecutor
 
 def download_images(urls, storage_path, index):
-	if index % 1000 == 0:
-		print("Tried downloading {} images, {} downloaded".format(index, storage_dir = storage_path))
+	if index % 20 == 0:
+		print(index)
 
 	if os.path.exists(storage_path):
-		return 2
+		try:
+			im = Image.open(storage_path)
+			im = np.array(im, dtype = np.float32)
+			return 2
+		except:
+			os.remove(storage_path)
 
 	for url in urls:
 		try:
-			urlretrieve(url, storage_path)
-			return 1
+			f = urllib.urlopen(url) 
+			with open(storage_path, "wb") as code:
+   				code.write(f.read())
+   			try:
+				im = Image.open(storage_path)
+				im = np.array(im, dtype = np.float32)
+				return 1
+			except:
+				os.remove(storage_path)
+				continue
 		except:
-			continue
+				continue
 
+#fgvc4_iMat.test.image
+#fgvc4_iMat.train.data
+#fgvc4_iMat.validation.data
 
 with open('fgvc4_iMat.test.image.json', 'r') as f:
 	data = json.load(f)
 	
 	urls_list = [item['url'] for item in data['images']]
-	path_names = ["test_data/" + item['imageId'] + ".jpg" for item in data['images']]
+	path_names = ["data/test_data/" + item['imageId'] + ".jpg" for item in data['images']]
 	indexes = [i for i in range(1, len(path_names)+1)]
 
-	#download_images(urls_list[0], path_names[0], indexes[0])
-	#map(download_images, urls_list, path_names, indexes)
 	with ThreadPoolExecutor(128) as executor:
 		executor.map(download_images, urls_list, path_names, indexes)
+
+print("finish!")
